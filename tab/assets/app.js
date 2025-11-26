60112d0b2665a5836f8c1858b2535b3df9cf8626
Anhelina Moroziuk <moroziuk14@gmail.com>
2025-11-26 23:08:04 +0100

    Add files via upload

--- tab/assets/app.js
+++ tab/assets/app.js
@@ -1,77 +1,39 @@
-"use strict"
-
+"use strict";
 
 const tabLinks = document.querySelectorAll(".tab__el a");
 
-
-if(tabLinks){
-    for(let tablink of tabLinks){
-        tablink.addEventListener("click",tabTrigger);
-    }
+if(tabLinks.length > 0){
+   for (let tabLink of tabLinks) {
+       tabLink.addEventListener("click", tabTrigger);
+   }
 }
 
 function tabTrigger(event){
+   // 1. Забороняємо стандартну дію (перехід по href)
+   event.preventDefault();
 
-    event.preventDefault();
+   // 2. Знаходимо батьківський контейнер .tab
+   const parentTab = event.currentTarget.closest(".tab");
 
-    const parentTab = event.currentTarget.closest(".tab");
+   // 3. Перемикаємо активний елемент навігації
+   const activeLink = parentTab.querySelector(".tab__el--active");
+   if(activeLink){
+      activeLink.classList.remove("tab__el--active");
+   }
+   event.currentTarget.parentElement.classList.add("tab__el--active");
 
+   // 4. Перемикаємо активний контент
+   const activeTab = parentTab.querySelector(".tab__content--active");
+   if(activeTab){
+      activeTab.classList.remove("tab__content--active");
+   }
 
-
-
-    const activeLink = parentTab.querySelector(".tab__el--active");
-    activeLink.classList.remove("tab__el--active");
-    event.currentTarget.parentNode.classList.add("tab__el--active");
-
-
-    const activeTab = parentTab.querySelector(".tab__content--active");
-    activeTab.classList.remove("tab__content--active");
-
-
-    const tabSelectID = event.currentTarget.getAttribute("href");
-    const tabSelect = parentTab.querySelector(tabSelectId);
-    tabSelect.classList.add("tab__content--active");
-
-
-
-
-
-
-
-//const pour selectionner les liens de ma .tab_nav > nodelist
-//boucler sur ma const
-
-//ajouter un eventlistener sur les liens pour le click
-
-//je lance une function
-
-//je declare ma function
-// premier chose a faire la classe de la nav
-
-//selectionner tab__el--active
-
-//supprimer la classe tab__el--active
-
-//ajouter la classe tab__el--active sur celui qui a ete clique
-// currentTarget
-
-//deuxieme chose a faire
-
-//selectionner tab__content--active
-
-//supprimer tab__content--active
-
-
-
-
-
-//ajouter la classe tab__content--active sur le bon div
-
-//recuperer le contenu de l'attribut href sur le currentTarget
-//selectionner le div qui a comme attribut la valeur de href
-//sur cet element j'ajoute la classe tab__content--active
-
-
+   const tabID = event.currentTarget.getAttribute("href");
+   const tabSelect = parentTab.querySelector(tabID);
+   if(tabSelect){
+      tabSelect.classList.add("tab__content--active");
+   }
 }
+
 
 
