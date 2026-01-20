6f1edda059566e8cabd22a841933b351db9b3400
Anhelina Moroziuk <moroziuk14@gmail.com>
2026-01-20 14:38:01 +0100

    slider

---
+++ slider/assets/app.js
@@ -0,0 +1,37 @@
+"use strict";
+
+
+const btnPrev = document.querySelector(".slider__btn--prev");
+const btnNext = document.querySelector(".slider__btn--next");
+
+btnPrev.addEventListener("click", prevSlide);
+btnNext.addEventListener("click", nextSlide);
+
+function nextSlide() {
+    const elShow = document.querySelector(".slider__el--show");
+    const elNext = elShow.nextElementSibling;
+
+    elShow.classList.remove("slider__el--show");
+    if(elNext){
+        elNext.classList.add("slider__el--show");
+    }else{
+        const elFirst = elShow.parentElement.firstElementChild;
+        elFirst.classList.add("slider__el--show");
+    }
+}
+
+function prevSlide() {
+    const elShow = document.querySelector(".slider__el--show");
+    const elPrev = elShow.previousElementSibling;
+
+    elShow.classList.remove("slider__el--show");
+    if(elPrev){
+        elPrev.classList.add("slider__el--show");
+    }else{
+        const elLast = elShow.parentElement.lastElementChild;
+        elLast.classList.add("slider__el--show");
+    }
+}
+
+
+
